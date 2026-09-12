8d714724b8e347786cdd7a18d61f5c691324ad4b
bikashghadai3 <42334411+bikashghadai3@users.noreply.github.com>
2026-09-12 19:44:15 +0530

    Banking project

---
+++ BankingTransactionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.bankingms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingTransactionServiceApplication.class, args);
+    }
+}
